0d01f80c9f72a3ba233cd7d550b809d8320baa06
Yu-Chen Chang <chang397@purdue.edu>
2016-08-01 20:57:56 -0400

    daily update

--- src/72_edit_distance.cpp
+++ src/72_edit_distance.cpp
@@ -24,6 +24,24 @@ public:
         }
         return cur[m]; 
     }
+	int minDistance_2d(string word1, string word2) {
+		int m = word1.size();
+		int n = word2.size();
+		vector<vector<int> > dp(m+1,vector<int>(n+1,0));
+		for(int i=0;i<=m;i++)
+			dp[i][0] = i;
+		for(int j=0;j<=n;j++)
+			dp[0][j] = j;
+
+		for(int i=1;i<=m;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				dp[i][j] = min(min(dp[i-1][j]+1,dp[i][j-1]+1),dp[i-1][j-1]+(word1[i-1]==word2[j-1]?0:1));
+			}
+		}
+		return dp[m][n];
+	}
 };
 
 
